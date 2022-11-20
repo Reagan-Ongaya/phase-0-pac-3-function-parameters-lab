b249836f75402c6dcaa56a23867c1a76a734c4d2
reaganongaya <reagan.ongaya@student.moringaschool.com>
2022-11-20 22:44:08 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('Hi,my name is ${Reagan}')
+
+function logTwoValues(Reagan, Ongaya) {console.log('The two values are ${Reagan} and ${Ongaya}.');
+}
+function sayHelloTo(firstName="User") { console.log('Hello, ${firstName}!');}
